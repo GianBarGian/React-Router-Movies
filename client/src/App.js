404166afd2de2939b2409540aefe4fdc8baa69ec
Giacomo Benati <giacomobenati@mailbox.org>
2019-02-25 14:55:11 +0100

    added save movie functionality

--- client/src/App.js
+++ client/src/App.js
@@ -19,12 +19,18 @@ export default class App extends Component {
     this.setState({ savedList });
   };
 
+  propMovie = props => {
+    return (
+      <Movie addToSavedList={this.addToSavedList} {...props}/>
+    )
+  }
+
   render() {
     return (
       <div>
         <SavedList list={this.state.savedList} />
         <Route exact path="/" component={MovieList} />
-        <Route path="/movies/:id" component={Movie} />
+        <Route path="/movies/:id" component={this.propMovie} />
       </div>
     );
   }
